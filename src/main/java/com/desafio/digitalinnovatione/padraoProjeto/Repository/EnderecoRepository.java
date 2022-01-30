d4bf8164085733c876c48fb414ddd1c44df2ed84
Charles Dennis <charlesdennis2009@hotmail.com>
2022-01-30 19:26:07 -0300

    adicionando a anotacao

--- src/main/java/com/desafio/digitalinnovatione/padraoProjeto/Repository/EnderecoRepository.java
+++ src/main/java/com/desafio/digitalinnovatione/padraoProjeto/Repository/EnderecoRepository.java
@@ -1,9 +1,11 @@
 package com.desafio.digitalinnovatione.padraoProjeto.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.desafio.digitalinnovatione.padraoProjeto.entity.Endereco;
 
+@Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, String> {
 
 	void deleteByCep(String cep);
