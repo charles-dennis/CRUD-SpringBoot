137fa3402a5809b2c2f482e9392acceb90af07ba
Charles Dennis <charlesdennis2009@hotmail.com>
2022-01-30 17:29:04 -0300

    criado as interfaces

---
+++ src/main/java/com/desafio/digitalinnovatione/padraoProjeto/Repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.desafio.digitalinnovatione.padraoProjeto.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.desafio.digitalinnovatione.padraoProjeto.entity.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
---
+++ src/main/java/com/desafio/digitalinnovatione/padraoProjeto/Repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.desafio.digitalinnovatione.padraoProjeto.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.desafio.digitalinnovatione.padraoProjeto.entity.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, String> {
+
+}
